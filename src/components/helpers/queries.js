3fef35115267ec8711f802ccd168072c9968fbbb
matiasfsd7 <matias.n.ortega@gmail.com>
2023-08-08 23:50:13 -0300

    agrue text area a login en mail y password

---
+++ src/components/helpers/queries.js
@@ -0,0 +1,29 @@
+const login = async (usuario) => {
+  try {
+    //pedir la lista de usuarios a json-server.
+
+    const respuesta = await fetch("http://localhost:3000/usuarios");
+    const listaUsuarios = await respuesta.json();
+
+    //buscar si el usuario que completo el formulario esta dentro de la lista del json-server.
+    const usuarioEncontrado = listaUsuarios.find(
+      (itemUsuario) => itemUsuario.email === usuario.email
+    );
+    // si encontre el usuario y si pass es correcto, esta perfecto.
+    if (usuarioEncontrado) {
+      if (usuarioEncontrado.password === usuario.password) {
+        console.log("todo esta perfecto");
+        return usuarioEncontrado;
+      } else {
+        console.log("el password es incorrecto");
+        return "usuario u/o contraseña incorrecto";
+      }
+    } else {
+      console.log("el mail es incorrecto");
+      return null;
+    }
+    //sino tendria que salir que es incorrecto.
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -8,8 +8,9 @@ const Login = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit0 = () => {
+  const onSubmit0 = (usuario) => {
     console.log("aqui agruego la logica");
+    console.log(usuario);
   };
 
   return (
@@ -20,17 +21,19 @@ const Login = () => {
           <Form onSubmit={handleSubmit(onSubmit0)}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Ingrese un email" />
-
-              {...register("email", {
-                required: "El email es un dato obligatorio",
-                pattern: {
-                  value:
-                    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-                  message:
-                    "El email debe cumplir un formato valido como el siguiente email@email.com",
-                },
-              })}
+              <Form.Control
+                type="email"
+                placeholder="Ingrese un email"
+                {...register("email", {
+                  required: "El email es un dato obligatorio",
+                  pattern: {
+                    value:
+                      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                    message:
+                      "El email debe cumplir un formato valido como el siguiente email@email.com",
+                  },
+                })}
+              />
               <Form.Text className="text-danger">
                 {errors.email?.message}
               </Form.Text>
@@ -38,7 +41,21 @@ const Login = () => {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                {...register("password", {
+                  required: "La contraseña es un dato obligatorio",
+                  pattern: {
+                    value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                    message:
+                      "La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
             </Form.Group>
             <Button variant="primary" type="submit">
               Ingresar
